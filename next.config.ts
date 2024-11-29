afb6b45c4a767ac4f2d9712b587e1c9f6fd46b63
viz <viz@noreply.gitcode.com>
2024-11-29 17:26:13 +0800

    add out

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,4 @@
-import type { NextConfig } from "@/node_modules/next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  distDir: 'dist', // 指定输出目录为 'dist'
-  reactStrictMode: true, // 启用 React 严格模式
-  swcMinify: true, // 使用 SWC 进行代码压缩
- 
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization.splitChunks.maxSize = 249856; // 设置最大块大小为 249.856 KiB
-    }
-    return config;
-  },
+module.exports = {
+  swcMinify: true, // 确保这个配置是正确的
+  // 其他配置
 };
-
-export default nextConfig;
