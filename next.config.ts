e63432774bba5faca0509831f757a140fb280e55
viz <viz@noreply.gitcode.com>
2024-11-29 11:09:44 +0800

    add dist

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "@/node_modules/next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir: 'dist', // 指定输出目录为 'dist'
 };
 
 export default nextConfig;
