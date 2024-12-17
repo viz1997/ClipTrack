e842be4d37286fa2c8183340ee9571d16106911f
viz <viz@noreply.gitcode.com>
2024-12-17 18:44:56 +0800

    Astro Version

---
+++ src/utils/data.ts
@@ -0,0 +1,130 @@
+import visibilityDilemma from "../assets/images/visibility-dilemma.png";
+import engagementGap from "../assets/images/engagement-gap.png";
+import resultsInHibernation from "../assets/images/results-in-hibernation.gif";
+
+const services = [
+  {
+    title: "智能剪贴",
+    features: [
+      "自动保存您复制的文本、链接和图片",
+    
+    ],
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" stroke-width="1.5" fill="currentColor" class="w-6 h-6"><path d="M256 0c17.7 0 32 14.3 32 32V42.4c93.7 13.9 167.7 88 181.6 181.6H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H469.6c-13.9 93.7-88 167.7-181.6 181.6V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V469.6C130.3 455.7 56.3 381.7 42.4 288H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H42.4C56.3 130.3 130.3 56.3 224 42.4V32c0-17.7 14.3-32 32-32zM107.4 288c12.5 58.3 58.4 104.1 116.6 116.6V384c0-17.7 14.3-32 32-32s32 14.3 32 32v20.6c58.3-12.5 104.1-58.4 116.6-116.6H384c-17.7 0-32-14.3-32-32s14.3-32 32-32h20.6C392.1 165.7 346.3 119.9 288 107.4V128c0 17.7-14.3 32-32 32s-32-14.3-32-32V107.4C165.7 119.9 119.9 165.7 107.4 224H128c17.7 0 32 14.3 32 32s-14.3 32-32 32H107.4zM256 224a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>`,
+  },
+  {
+    title: "快速搜索",
+    features: [
+      "轻松查找和管理您保存的所有内容",
+     
+    ],
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" stroke-width="1.5" fill="currentColor" class="w-6 h-6"><path d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm306.7 69.1L162.4 380.6c-19.4 7.5-38.5-11.6-31-31l55.5-144.3c3.3-8.5 9.9-15.1 18.4-18.4l144.3-55.5c19.4-7.5 38.5 11.6 31 31L325.1 306.7c-3.2 8.5-9.9 15.1-18.4 18.4zM288 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg>`,
+  },
+  {
+    title: "源链接保存",
+    features: [
+      "动记录内容的来源网页，方便回溯",
+    
+    ],
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" stroke-width="1.5" fill="currentColor" class="w-6 h-6"><path d="M544 248v3.3l69.7-69.7c21.9-21.9 21.9-57.3 0-79.2L535.6 24.4c-21.9-21.9-57.3-21.9-79.2 0L416.3 64.5c-2.7-.3-5.5-.5-8.3-.5H296c-37.1 0-67.6 28-71.6 64H224V248c0 22.1 17.9 40 40 40s40-17.9 40-40V176c0 0 0-.1 0-.1V160l16 0 136 0c0 0 0 0 .1 0H464c44.2 0 80 35.8 80 80v8zM336 192v56c0 39.8-32.2 72-72 72s-72-32.2-72-72V129.4c-35.9 6.2-65.8 32.3-76 68.2L99.5 255.2 26.3 328.4c-21.9 21.9-21.9 57.3 0 79.2l78.1 78.1c21.9 21.9 57.3 21.9 79.2 0l37.7-37.7c.9 0 1.8 .1 2.7 .1H384c26.5 0 48-21.5 48-48c0-5.6-1-11-2.7-16H432c26.5 0 48-21.5 48-48c0-12.8-5-24.4-13.2-33c25.7-5 45.1-27.6 45.2-54.8v-.4c-.1-30.8-25.1-55.8-56-55.8c0 0 0 0 0 0l-120 0z"/></svg>`,
+  },
+ 
+ 
+];
+
+const solutions = [
+  {
+    title: "Floating Window & Sidebar",
+    painPoint:
+      "The floating window mode allows for easy adjustment of position, offering flexibility and convenience.",
+    agitatepainPoint:
+      "The sidebar mode does not obstruct the main content, providing intuitive operation.",
+    solution:
+      "Choose your preferred method.",
+    img: visibilityDilemma,
+  },
+  {
+    title: "Easy to Operate",
+    painPoint:
+      "Intuitive Display, Convenient Operation.",
+    agitatepainPoint:
+      "Help you quickly search, operate, and manage clipboard content.",
+    solution:
+      "Choose your preferred method.",
+    img: engagementGap,
+  },
+  {
+    title: "Seamless and Efficient Recording",
+    painPoint:
+      "No additional actions required, seamless recording ensures a smooth experience for you.",
+    agitatepainPoint:
+      "Original web links are preserved, allowing you to quickly locate the source and enhance productivity.",
+    solution:
+      "Accurate timestamps are included for easy tracking and management.",
+    img: resultsInHibernation,
+  },
+];
+
+const faqs = [
+  {
+    question: "ClipTrack是什么？",
+    answer:
+      "Social media marketing can significantly benefit your business by increasing brand awareness, engaging your target audience, driving website traffic, and generating leads. It also allows for direct interaction with your customers, fostering brand loyalty and providing valuable insights into consumer behavior.",
+  },
+  {
+    question: "ClipTrack支持哪些浏览器？",
+    answer:
+      "ClipTrack支持所有主流浏览器，包括但不限于Chrome、Firefox、Safari、Edge等。我们致力于为用户提供跨平台、无缝的剪贴板管理体验。",
+  },
+  {
+    question: "我的剪贴内容安全吗？",
+    answer:
+      "ClipTrack非常重视您的隐私和数据安全。我们采用端到端加密技术，确保您的剪贴内容在传输和存储过程中得到严格保护。同时，我们严格遵守相关法律法规，确保您的数据安全。",
+  },
+ 
+];
+
+
+const socialLinks = [
+  {
+    icon: "facebook",
+    path: "M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z",
+    href: "#"
+  },
+  {
+    icon: "linkedin",
+    path: "M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z",
+    href: "#"
+  },
+  {
+    icon: "twitter",
+    path: "M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z",
+    href: "#"
+  },
+  {
+    icon: "github",
+    path: "M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z",
+    href: "#"
+  }
+];
+
+const footerSupport = [
+  { itemText: "Pricing", itemLink: "#" },
+  { itemText: "Guides", itemLink: "#" },
+  { itemText: "FAQ", itemLink: "#" },
+  { itemText: "Contact", itemLink: "#" }
+];
+
+const footerProduct = [
+  { itemText: "Features", itemLink: "#" },
+  { itemText: "How It Works", itemLink: "#" },
+  { itemText: "FAQs", itemLink: "#" }
+
+  
+];
+
+const footerLegal = [
+  { itemText: "Privacy", itemLink: "/privacy" },
+  { itemText: "Terms", itemLink: "/terms" }
+];
+
+export { services, solutions, faqs, footerSupport, footerProduct, footerLegal, socialLinks};
